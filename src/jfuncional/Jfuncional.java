495ba04a6370f6508ed3fcbd078ca6c6799bb646
Javier Carchipulla S. <krchipullajavier@gmail.com>
2025-04-04 17:09:03 -0500

    BinaryOperator - es una función funcional que acepta 2 parámetros del mismo tipo y devuelve un resultado del mismo tipo. - permite el uso de Integer, Double, String.. y solo acepta 2 parámetros - se puede usar para sumas, comparar, reducir listas y multiplicar - una lambda se ejecuta con el método .apply, si no se implementa la función solo retorna una referencia

--- src/jfuncional/Jfuncional.java
+++ src/jfuncional/Jfuncional.java
@@ -1,22 +1,45 @@
 package jfuncional;
 
+import java.util.List;
+import java.util.function.BinaryOperator;
+
 public class Jfuncional {
 
     public static void main(String[] args) {
         
+        // definimos una lambda que emplemente BinaryOperator<>
+        BinaryOperator<Integer> max = (a,b)-> a > b ?a:b;
+        System.out.println(max.apply(10,05));
+        
+        // Strings
+        BinaryOperator<String> cadena = (c,d)-> c+""+d;
+        System.out.println(cadena.apply("pu", "erta"));
+        
+        // reducir elementos de una lista 
+        List<Integer> numeros =List.of(9,7,5,6,7);
+        BinaryOperator<Integer> suma = (j, s) -> j + s;
+        
+        int resultado = numeros.stream().reduce(0, suma);
+        System.out.println(resultado);
+        
+        
+        
+       
         //tipos de lambdas
         
-        // sin parametros
-        ()-> System.out.println("Hola");
-        
-        //tiene un parametro y un bloque de codigo
-        x -> x*x
-          
-        // varios parametros        
-        (a,b) ->a+b
-         
-        // con tipo explicito        
-        (String nombre) -> {return nombre.length}
+//        //tipos de lambdas
+//        
+//        // sin parametros
+//        ()-> System.out.println("Hola");
+//        
+//        //tiene un parametro y un bloque de codigo
+//        x -> x*x
+//          
+//        // varios parametros        
+//        (a,b) ->a+b
+//         
+//        // con tipo explicito        
+//        (String nombre) -> {return nombre.length}
         
     }
     
