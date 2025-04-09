b16ee8a307b6cc78bc9916443eb41f3f97044e4a
Javier Carchipulla S. <krchipullajavier@gmail.com>
2025-04-08 20:04:15 -0500

    Interfaz funcional Functio<T,R> - uso de metodos utiles como .andThen(Funtion after) - uso de .andThen - Transforma primero con esta función y luego aplica otra. - uso de .compose - Primero aplica la función que le pasas, luego la actual.

--- src/jfuncional/Princip.java
+++ src/jfuncional/Princip.java
@@ -5,20 +5,45 @@ import java.util.function.Function;
 public class Princip {
     public static void main(String[] args) {
         
-        // ejemplo de obtener la longitud de una cadena
-        Function<String, Integer> conversion = precio -> precio.length();
-        System.out.println(conversion.apply("Java"));
-        System.out.println(conversion.apply("Juanin"));
-//        
-        //
-        Function<Integer,String> conver = nota ->{
-            if (nota >= 90)return "exelente";
-            else if (nota >= 70)return "bueno";    
-            else return "Insuficiente";
-        };
         
-        System.out.println(conver.apply(95)); // Excelente
-        System.out.println(conver.apply(65)); // Insuficiente
+        
+        
+        // aplicacamos la primera funcion y luego aplica otro
+        
+        //F1
+        Function<Integer, Integer> doble = n -> n*2;
+        //F2
+        Function<Integer, String> convertir = n -> "resultado: "+n;
+        //F3 esta tercera funcion combina las 2 funciones anteriores
+        Function<Integer, String> combinado = doble.andThen(convertir);
+        
+        System.out.println(combinado.apply(5));
+        
+        
+        // aplicamos primero la segunda funcion y despues la primera funcion
+        Function<String, String> quitarEspacios = s -> s.trim();
+        Function<String, Integer> contarCaract = s -> s.length();
+        
+        Function<String, Integer> limpiaYCuenta = contarCaract.compose(quitarEspacios);
+        System.out.println(limpiaYCuenta.apply("Hola que"));
+        
+        
+        
+        
+        // ejemplo de obtener la longitud de una cadena
+//        Function<String, Integer> conversion = precio -> precio.length();
+//        System.out.println(conversion.apply("Java"));
+//        System.out.println(conversion.apply("Juanin"));
+//        
+        //presentacion de un mensaje en un determinado rango
+//        Function<Integer,String> conver = nota ->{
+//            if (nota >= 90)return "exelente";
+//            else if (nota >= 70)return "bueno";    
+//            else return "Insuficiente";
+//        };
+        
+//        System.out.println(conver.apply(95)); // Excelente
+//        System.out.println(conver.apply(65)); // Insuficiente
                 
     }
     
