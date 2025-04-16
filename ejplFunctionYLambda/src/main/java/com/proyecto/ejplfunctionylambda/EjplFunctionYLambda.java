4490b5bc5fe52b4aceb79c433276146006d4393b
Javier Carchipulla S. <krchipullajavier@gmail.com>
2025-04-16 17:27:15 -0500

    ejercicio de Funtion y una lambda - ejercicio de obtener el salario final de una persona

--- ejplFunctionYLambda/src/main/java/com/proyecto/ejplfunctionylambda/EjplFunctionYLambda.java
+++ ejplFunctionYLambda/src/main/java/com/proyecto/ejplfunctionylambda/EjplFunctionYLambda.java
@@ -1,26 +1,36 @@
 package com.proyecto.ejplfunctionylambda;
 
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class EjplFunctionYLambda {
 
     public static void main(String[] args) {
-        List<Estudiante> estudiantes = List.of(
-            new Estudiante("Ana", 80, 90),
-            new Estudiante("Luis", 60, 70)
-        );
+        
+        BiFunction<Double, Double, Double> clclSalarioOfil = (salario, bono) ->
+                salario +(salario*bono);
+        
+        Double resultado = clclSalarioOfil.apply(1000.0, 0.15);
+        
+        System.out.println("El salario Final es: "+resultado);
         
         
-        //
-        Function<Estudiante, String> calcularNTF = est ->{
-            int promedio = (est.examen1 + est.examen2);        
-            return est.nombre+" -> nota final: "+promedio;
-        };
-        
-        estudiantes.stream()
-                .map(calcularNTF)
-                .forEach(System.out::println);
+//        List<Estudiante> estudiantes = List.of(
+//            new Estudiante("Ana", 80, 90),
+//            new Estudiante("Luis", 60, 70)
+//        );
+//        
+//        
+//        //
+//        Function<Estudiante, String> calcularNTF = est ->{
+//            int promedio = (est.examen1 + est.examen2);        
+//            return est.nombre+" -> nota final: "+promedio;
+//        };
+//        
+//        estudiantes.stream()
+//                .map(calcularNTF)
+//                .forEach(System.out::println);
         
     }
 }
